3cd7603e975ad2710fe4749ba957446e58aa5a42
Levin Jian <jianzhirong@gmail.com>
2013-11-30 22:17:41 +0800

    added the login and home page

--- app/js/app.js
+++ app/js/app.js
@@ -3,14 +3,39 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+        config(['$routeProvider', function($routeProvider) {
+        var access = routingConfig.accessLevels;
+        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+        $routeProvider.when('/view3/:chapterId/Section/:sectionId', {templateUrl: 'partials/book.html', controller: 'MyCtrl3'});
+        $routeProvider.when('/login', {
+            templateUrl: 'partials/login.html', 
+            controller: 'MyCtrl4',
+            access:         access.anon
+        });
+        $routeProvider.when('/home', {
+            templateUrl: 'partials/home.html', 
+            controller: 'HomeCtrl',
+            access:         access.user
+        });
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]).
+        run(['$rootScope', '$location', 'Auth', function($rootScope, $location, Auth) {
+
+        $rootScope.$on("$routeChangeStart", function(event, next, current) {
+            $rootScope.error = null;
+            if (!Auth.authorize(next.access)) {
+                if (Auth.isLoggedIn())
+                    $location.path('/');
+                else
+                    $location.path('/login');
+            }
+        });
+
+    }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,25 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
+  .controller('MyCtrl4', ['$scope','$location','$route','$routeParams','version',
+      function($scope,$location,$route, $routeParams, version) {
+        $scope.greeting = 'hurray';
+        $scope.locationpath = $location.path();
+        $scope.location = $location;
+         $scope.routeParams = $routeParams;
+  }])
+  .controller('HomeCtrl', ['$scope','$location','$route','$routeParams','version',
+      function($scope,$location,$route, $routeParams, version) {
+
+  }])
+  .controller('MyCtrl3', ['$scope','$location','$route','$routeParams','version',
+      function($scope,$location,$route, $routeParams, version) {
+        $scope.greeting = 'hurray';
+        $scope.locationpath = $location.path();
+        $scope.location = $location;
+         $scope.routeParams = $routeParams;
+         $location.path('/view2');
+  }])
   .controller('MyCtrl2', [function() {
 
   }]);
