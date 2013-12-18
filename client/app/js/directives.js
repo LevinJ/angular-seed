617550810189d8067385cb0ee584c46825d373f1
Levin Jian <jianzhirong@gmail.com>
2013-12-18 15:31:08 +0800

    add user switch on route page

--- client/app/js/directives.js
+++ client/app/js/directives.js
@@ -4,65 +4,88 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
-  
-  
-  'use strict';
+        directive('appVersion', ['version', function(version) {
+                return function(scope, elm, attrs) {
+                    elm.text(version);
+                };
+            }]);
+
+
+'use strict';
 
 angular.module('myApp.directives')
-.directive('accessLevel', ['Auth', function(Auth) {
-    return {
-        restrict: 'A',
-        link: function($scope, element, attrs) {
-            var prevDisp = element.css('display')
-                , userRole
-                , accessLevel;
+        .directive('loading', ['$http', function($http)
+            {
+                return {
+                    restrict: 'A',
+                    link: function(scope, elm, attrs)
+                    {
+                        scope.isLoading = function() {
+                            return $http.pendingRequests.length > 0;
+                        };
 
-            $scope.user = Auth.user;
-            $scope.$watch('user', function(user) {
-                if(user.role)
-                    userRole = user.role;
-                updateCSS();
-            }, true);
+                        scope.$watch(scope.isLoading, function(v)
+                        {
+                            if (v) {
+                                elm.show();
+                            } else {
+                                elm.hide();
+                            }
+                        });
+                    }
+                };
 
-            attrs.$observe('accessLevel', function(al) {
-                if(al) accessLevel = $scope.$eval(al);
-                updateCSS();
-            });
+            }])
+        .directive('accessLevel', ['Auth', function(Auth) {
+                return {
+                    restrict: 'A',
+                    link: function($scope, element, attrs) {
+                        var prevDisp = element.css('display')
+                                , userRole
+                                , accessLevel;
 
-            function updateCSS() {
-                if(userRole && accessLevel) {
-                    if(!Auth.authorize(accessLevel, userRole))
-                        element.css('display', 'none');
-                    else
-                        element.css('display', prevDisp);
-                }
-            }
-        }
-    };
-}]);
+                        $scope.user = Auth.user;
+                        $scope.$watch('user', function(user) {
+                            if (user.role)
+                                userRole = user.role;
+                            updateCSS();
+                        }, true);
+
+                        attrs.$observe('accessLevel', function(al) {
+                            if (al)
+                                accessLevel = $scope.$eval(al);
+                            updateCSS();
+                        });
+
+                        function updateCSS() {
+                            if (userRole && accessLevel) {
+                                if (!Auth.authorize(accessLevel, userRole))
+                                    element.css('display', 'none');
+                                else
+                                    element.css('display', prevDisp);
+                            }
+                        }
+                    }
+                };
+            }]);
 
 angular.module('myApp.directives').directive('activeNav', ['$location', function($location) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var nestedA = element.find('a')[0];
-            var path = nestedA.href;
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var nestedA = element.find('a')[0];
+                var path = nestedA.href;
 
-            scope.location = $location;
-            scope.$watch('location.absUrl()', function(newPath) {
-                if (path === newPath) {
-                    element.addClass('active');
-                } else {
-                    element.removeClass('active');
-                }
-            });
-        }
+                scope.location = $location;
+                scope.$watch('location.absUrl()', function(newPath) {
+                    if (path === newPath) {
+                        element.addClass('active');
+                    } else {
+                        element.removeClass('active');
+                    }
+                });
+            }
 
-    };
+        };
 
-}]);
+    }]);
